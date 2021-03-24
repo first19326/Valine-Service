573770c2a4ae331f35137e974b2ba4a60b6271f1
first19326 <870390039@qq.com>
2021-03-24 20:24:33 +0800

    create valine service

---
+++ utilities/check-spam.js
@@ -0,0 +1,106 @@
+'use strict';
+const AV = require('leanengine');
+const akismet = require('akismet-api');
+const akismetClient = akismet.client({
+    key  : process.env.AKISMET_KEY,
+    blog : process.env.SITE_URL
+});
+
+exports.checkSpam = (comment, ip)=> {
+    if (process.env.AKISMET_KEY === 'MANUAL_REVIEW') {
+        console.log('已使用人工审核模式, 评论审核后才会发表.');
+        comment.setACL(new AV.ACL({"*":{"read":false}}));
+        comment.set('isSpam', true);
+        comment.save();
+        return;
+    }
+    akismetClient.verifyKey(function(err, valid) {
+        if (err) console.log('Akismet key 异常: ', err.message);
+        if (valid) {
+            // TODO 这里有缺陷
+            comment.set('ip', ip);
+            akismetClient.checkSpam({
+                user_ip : ip,
+                user_agent : comment.get('ua'),
+                referrer : process.env.SITE_URL + comment.get('url'),
+                permalink : process.env.SITE_URL + comment.get('url'),
+                comment_type : 'comment',
+                comment_author : comment.get('nick'),
+                comment_author_email : comment.get('mail'),
+                comment_author_url : comment.get('link'),
+                comment_content : comment.get('comment'),
+                // is_test : true // Default value is false
+            }, function(err, spam) {
+                if (err) console.log (`垃圾评论检测出错! ${err}`);
+                if (spam) {
+                    console.log('这是一条垃圾评论, 建议删除!');
+                    comment.set('isSpam', true);
+                    comment.setACL(new AV.ACL({"*":{"read":false}}));
+                    comment.save();
+                } else {
+                    comment.set('isSpam', false);
+                    comment.setACL(new AV.ACL({"*":{"read":true}}));
+                    comment.save();
+                    console.log('垃圾评论检测完成, 正常!');
+                }
+            });
+        }
+        else console.log('未开启垃圾评论过滤或 Akismet key 异常!');
+    });
+};
+exports.submitSpam = (comment)=> {
+    if (process.env.AKISMET_KEY === 'MANUAL_REVIEW') {
+        return;
+    }
+    akismetClient.verifyKey(function(err, valid) {
+        if (err) console.log('Akismet key 异常: ', err.message);
+        if (valid) {
+            let ipAddr = comment.get('ip');
+            akismetClient.submitSpam({
+                user_ip : ipAddr,
+                user_agent : comment.get('ua'),
+                referrer : process.env.SITE_URL + comment.get('url'),
+                permalink : process.env.SITE_URL + comment.get('url'),
+                comment_type : 'comment',
+                comment_author : comment.get('nick'),
+                comment_author_email : comment.get('mail'),
+                comment_author_url : comment.get('link'),
+                comment_content : comment.get('comment'),
+                // is_test : true // Default value is false
+            }, function(err) {
+                if (!err) {
+                    console.log('垃圾评论已经提交!');
+                }
+            });
+        }
+        else console.log('未开启垃圾评论过滤或 Akismet key 异常!');
+    });
+};
+exports.submitHam = (comment)=> {
+    if (process.env.AKISMET_KEY === 'MANUAL_REVIEW') {
+        return;
+    }
+    akismetClient.verifyKey(function(err, valid) {
+        if (err) console.log('Akismet key 异常: ', err.message);
+        if (valid) {
+            let ipAddr = comment.get('ip');
+            akismetClient.submitHam({
+                user_ip : ipAddr,
+                user_agent : comment.get('ua'),
+                referrer : process.env.SITE_URL + comment.get('url'),
+                permalink : process.env.SITE_URL + comment.get('url'),
+                comment_type : 'comment',
+                comment_author : comment.get('nick'),
+                comment_author_email : comment.get('mail'),
+                comment_author_url : comment.get('link'),
+                comment_content : comment.get('comment'),
+                // is_test : true // Default value is false
+            }, function(err) {
+                if (!err) {
+                    console.log('评论已经标记为正常评论!');
+                }
+            });
+        }
+        else console.log('未开启垃圾评论过滤或 Akismet key 异常!');
+    });
+};
---
+++ utilities/send-mail.js
@@ -0,0 +1,101 @@
+'use strict';
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const fs  = require('fs');
+const path = require('path');
+
+let config = {
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+    }
+}
+
+if (process.env.SMTP_SERVICE != null) {
+    config.service = process.env.SMTP_SERVICE;
+} else {
+    config.host = process.env.SMTP_HOST;
+    config.port = parseInt(process.env.SMTP_PORT);
+    config.secure = process.env.SMTP_SECURE === "false" ? false : true;
+}
+
+const transporter = nodemailer.createTransport(config);
+let templateName = process.env.TEMPLATE_NAME ?  process.env.TEMPLATE_NAME : "default";
+let noticeTemplate = ejs.compile(fs.readFileSync(path.resolve(process.cwd(), 'template', templateName, 'notice.ejs'), 'utf8'));
+let sendTemplate = ejs.compile(fs.readFileSync(path.resolve(process.cwd(), 'template', templateName, 'send.ejs'), 'utf8'));
+
+transporter.verify(function(error, success) {
+    if (error) {
+        console.log('SMTP 邮箱配置异常: ', error);
+    }
+    if (success) {
+        console.log("SMTP 邮箱配置正常!");
+    }
+});
+
+exports.notice = (comment) => {
+
+    // Master自己发的评论不需要通知
+    if (comment.get('mail') === process.env.RECEIVE_EMAIL || comment.get('mail') === process.env.SMTP_USER) {
+        return;
+    }
+
+    let emailSubject = '叮咚！『' + process.env.SITE_NAME + '』上有新评论了';
+    let emailContent =  noticeTemplate({
+                        siteName: process.env.SITE_NAME,
+                        siteUrl: process.env.SITE_URL,
+                        name: comment.get('nick'),
+                        text: comment.get('comment'),
+                        url: process.env.SITE_URL + comment.get('url')
+                    });
+
+    if (comment.get('isSpam')) {
+        emailSubject = '[SPAM]' + emailSubject;
+    }
+
+    let mailOptions = {
+        from: '"' + process.env.SENDER_NAME + '" <' + process.env.SMTP_USER + '>',
+        to: process.env.RECEIVE_EMAIL || process.env.SMTP_USER,
+        subject: emailSubject,
+        html: emailContent
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        comment.set('isNotified', true);
+        comment.save();
+        console.log('Master通知邮件成功发送: %s', info.response);
+    });
+}
+
+exports.send = (currentComment, parentComment)=> {
+
+    let emailSubject = '叮咚！『' + process.env.SITE_NAME + '』上有人@了你';
+    let emailContent = sendTemplate({
+                            siteName: process.env.SITE_NAME,
+                            siteUrl: process.env.SITE_URL,
+                            pname: parentComment.get('nick'),
+                            ptext: parentComment.get('comment'),
+                            name: currentComment.get('nick'),
+                            text: currentComment.get('comment'),
+                            url: process.env.SITE_URL + currentComment.get('url') + "#" + currentComment.get('pid')
+                        });
+
+    let mailOptions = {
+        from: '"' + process.env.SENDER_NAME + '" <' + process.env.SMTP_USER + '>', // sender address
+        to: parentComment.get('mail'),
+        subject: emailSubject,
+        html: emailContent
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        currentComment.set('isNotified', true);
+        currentComment.save();
+        console.log('AT通知邮件成功发送: %s', info.response);
+    });
+};
